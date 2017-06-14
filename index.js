d5bd1c3d79e174a4749862eae3123be60d1a0e0c
xcstream <cheng.xu@all-dream.com>
2017-06-14 21:39:42 +0800

    starter

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const session = require('express-session')
+const bodyParser = require("body-parser");
+const serveStatic = require('serve-static')
+const static = serveStatic('public' )
+app.set('view engine', 'pug')
+
+var helmet = require('helmet');
+app.use(helmet());
+
+app.use(session({
+    secret: 'www.51jinkela.com',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false ,maxAge:1000*3600*24}
+}))
+
+app.use(bodyParser.json({ type: 'application/json' }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(static);
+
+var port = 4500
+app.listen(port)
+console.log('http://localhost:'+port)
