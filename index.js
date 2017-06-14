d8f3f47f8130aa36d18348da2c89f8f3e0295597
xcstream <cheng.xu@all-dream.com>
2017-06-14 21:45:54 +0800

    config

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const session = require('express-session')
 const bodyParser = require("body-parser");
 const serveStatic = require('serve-static')
 const static = serveStatic('public' )
+
 app.set('view engine', 'pug')
-
 var helmet = require('helmet');
-app.use(helmet());
 
+app.use(helmet());
 app.use(session({
     secret: 'www.51jinkela.com',
     resave: true,
     saveUninitialized: true,
-    cookie: { secure: false ,maxAge:1000*3600*24}
+    cookie: {secure: true, maxAge: 87654321}
 }))
 
 app.use(bodyParser.json({ type: 'application/json' }))
