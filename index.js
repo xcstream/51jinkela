9eb7a6f79191dc21478ed16400d58cc4d2c33980
xcstream <cheng.xu@all-dream.com>
2017-06-14 22:29:36 +0800

    改中文吧

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const 网站 = require('express')()
-const 会话 = require('express-session')
-const 解析器 = require("body-parser");
-const 静态文件服务 = require('serve-static')
-const 静态文件目录 = 静态文件服务('public' )
-const 头盔 = require('helmet');
-
+网站 = require('express')()
+会话 = require('express-session')
+解析器 = require("body-parser");
+静态文件服务 = require('serve-static')
+静态文件目录 = 静态文件服务('public' )
+头盔 = require('helmet');
 网站.set('view engine', 'pug')
-
 网站.use(头盔());
 网站.use(会话({
     secret: '美国圣地亚哥' + Math.random(),
